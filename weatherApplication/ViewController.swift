527ab7f43a6d98e196ce78702b5ea8eeca0f0957
henrykchan <chan.henryk@gmail.com>
2017-03-30 22:40:09 -0400

    First commit

---
+++ weatherApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApplication
+//
+//  Created by Henry Chan on 3/30/17.
+//  Copyright © 2017 Henry Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
