8e09f7e3ba3723d292af765ee8e73936a85aa2a4
henrykchan <chan.henryk@gmail.com>
2017-04-02 00:02:41 -0400

    Added model

--- weatherApplication/ForecastModel.swift
+++ weatherApplication/ForecastModel.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+struct Forecast {
+    
+    var timezone: String?
+    var time: Date?
+    var timeString: String?
+    var icon: String?
+    var summary: String?
+    var temperature: Double?
+    var humidity: Double?
+    
+    init(dictionary: [String:Any]) {
+        
+        // Unwrapping time
+        if let timeDictionary = dictionary["time"] as? Double {
+            self.time = Date(timeIntervalSince1970: timeDictionary)
+            
+            // Date Formatter for title string
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "hh"
+            
+            if let unwrappedTime = self.time {
+                self.timeString = dateFormatter.string(from: unwrappedTime)
+            }
+        }
+        
+        // Unwrapping summary
+        if let summaryDictionary = dictionary["summary"] as? String {
+            self.summary = summaryDictionary
+        }
+        
+        // Unwrapping icon
+        if let iconDictonary = dictionary["icon"] as? String {
+            self.icon = iconDictonary
+        }
+        
+        // Unwrapping temperature
+        
+        if let temperatureDictionary = dictionary["temperature"] as? Double {
+            self.temperature = temperatureDictionary
+        }
+        
+        if let temperatureHumidity = dictionary ["humidity"] as? Double {
+            self.humidity = temperatureHumidity
+        }
+        
+    }
+}
